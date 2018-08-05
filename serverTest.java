57e10c1868825ae6c8fc39df48cf0755823c5b88
Rishan Mascarenhas <rishanmascarenhas@gmail.com>
2018-08-05 15:58:25 +0530

    Delete serverTest.java

--- serverTest.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.JFrame; 
-
-public class serverTest {
-	public static void main(String args[]) {
-		server sally = new server();
-		sally.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		sally.startRunning();
-	}
-}
